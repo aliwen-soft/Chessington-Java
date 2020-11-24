9ef907dc871d017fba5fc50584eb4122630fe90c
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-24 14:04:54 +0000

    Bishop RedStage

--- src/test/java/training/chessington/model/pieces/BishopTest.java
+++ src/test/java/training/chessington/model/pieces/BishopTest.java
@@ -1,7 +1,123 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class BishopTest {
+    @Test
+    public void bishopCanMoveDiagonally() {
+        // Arrange
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(3, 4);
+        board.placePiece(coords, bishop);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(2, 2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(3, 3)));
+        assertThat(moves).contains(new Move(coords, coords.plus(4, 4)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, -2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-3, -3)));
+    }
+
+
+    @Test
+    public void bishopCannotMoveThroughAPiece() {
+        // Arrange
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Piece king = new King(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(3, 4);
+        Coordinates kingcoords = new Coordinates(5, 6);
+        board.placePiece(coords, bishop);
+        board.placePiece(kingcoords, king);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(2, 2)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(3, 3)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(4, 4)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2, -2)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-3, -3)));
+    }
+
+    @Test
+    public void bishopCannotMoveVertically() {
+        // Arrange
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(3, 4);
+        board.placePiece(coords, bishop);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(1, 0)));
+    }
+
+    @Test
+    public void bishopCannotMoveHorizontally() {
+        // Arrange
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(3, 4);
+        board.placePiece(coords, bishop);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(0, 1)));
+    }
+
+    @Test
+    public void bishopCannotMoveOffTheBoard() {
+        // Arrange
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(7, 7);
+        board.placePiece(coords, bishop);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(1, 1)));
+    }
+
+    @Test
+    public void bishopCanTakePieces() {
+        // Arrange
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Piece king = new King(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(3, 4);
+        Coordinates kingcoords = new Coordinates(5, 6);
+        board.placePiece(coords, bishop);
+        board.placePiece(kingcoords, king);
+
+        // Act
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(2, 2)));
+    }
 }
