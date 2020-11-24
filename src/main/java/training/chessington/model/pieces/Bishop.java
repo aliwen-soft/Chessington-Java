7c7b4ef747f9f72dabbc23077b776871a76d4d54
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-24 14:09:21 +0000

    Bishop GreenStage- bishop can move take peices diagonally

--- src/main/java/training/chessington/model/pieces/Bishop.java
+++ src/main/java/training/chessington/model/pieces/Bishop.java
@@ -15,6 +15,24 @@ public class Bishop extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> moves = new ArrayList<>();
+        int[] modifiers = {-1,1};
+        for (int modifier : modifiers) {
+            boolean clearBackwards=true;
+            boolean clearRow=true;
+            for (int i = 1; i < 8; i++) {
+                Coordinates to = from.plus(i, modifier*i);
+                clearRow =  to.friendlySpace(board,colour) ? false : clearRow;
+                if (clearRow &&(to.emptySpace(board) || to.attackableSpace(board, colour))) {
+                    moves.add(new Move(from, to));
+                }
+                to = from.plus(-i, modifier*i);
+                clearBackwards =  to.friendlySpace(board,colour) ? false : clearBackwards;
+                if (clearBackwards &&(to.emptySpace(board) || to.attackableSpace(board, colour))) {
+                    moves.add(new Move(from, to));
+                }
+            }
+        }
+        return moves;
     }
 }
