ae43ecd1b762af8740c86f878cf85c67023ebcf6
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-24 13:42:20 +0000

    redstage - rook can move Vertically and Horizontally

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -15,6 +15,20 @@ public class Rook extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> moves = new ArrayList<>();
+        int[] modifiers = {-1,1};
+        for (int modifier : modifiers) {
+            for (int i = 0; i < 8; i++) {
+                Coordinates to = from.plus(modifier*i, 0);
+                if (to.emptySpace(board) || to.attackableSpace(board, colour)) {
+                    moves.add(new Move(from, to));
+                }
+                to = from.plus(0, modifier*i);
+                if (to.emptySpace(board) || to.attackableSpace(board, colour)) {
+                    moves.add(new Move(from, to));
+                }
+            }
+        }
+        return moves;
     }
 }
